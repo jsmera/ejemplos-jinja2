417cc7139c12fb0e9749b40691b002dd14e2de5a
quienesmera <merac1999@gmail.com>
2017-05-06 17:37:57 -0500

    Generando pdf con WeasyPrint

--- filtros/main.py
+++ filtros/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, json
 from jinja2 import Environment, FileSystemLoader
+from weasyprint import HTML
 
 
 
@@ -21,3 +22,5 @@ file_render = file.render(title= 'Playlist-chan', songs=songs).encode("utf-8")
 
 with open("filtros_render.html", "w") as f:
   f.write(file_render)
+
+HTML(string=file_render).write_pdf("filtros.pdf", stylesheets=["style.css"])
