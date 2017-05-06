622b0ca81582e64ba01aab1f44e66785059d6123
quienesmera <merac1999@gmail.com>
2017-05-06 17:03:49 -0500

    Ejemplos con filtros

---
+++ filtros/main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os, json
+from jinja2 import Environment, FileSystemLoader
+
+
+
+html_jinja2_env = Environment(
+  loader = FileSystemLoader(os.path.abspath('.')),
+  trim_blocks = True
+)
+
+# Cargando el json
+
+with open('playlist.json') as json_data:
+  d = json.load(json_data)
+
+songs = d['items']
+
+file = html_jinja2_env.get_template('filtros.html')
+file_render = file.render(title= 'Playlist-chan', songs=songs).encode("utf-8")
+
+with open("filtros_render.html", "w") as f:
+  f.write(file_render)
